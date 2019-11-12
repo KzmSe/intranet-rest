25dd01711e99d698c884836c25a40e4973cd5fd9
kzmse <sanan_kazimov@outlook.com>
2019-11-12 11:04:51 +0400

    initial commit

---
+++ src/main/java/az/gov/adra/IntranetRestApplication.java
@@ -0,0 +1,13 @@
+package az.gov.adra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntranetRestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/az/gov/adra/config/WebSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package az.gov.adra.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+
+}
