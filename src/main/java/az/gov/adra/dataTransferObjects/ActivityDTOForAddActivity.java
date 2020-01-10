f0b1c7b2c037157fc75ee16db436f2dd0e053442
kzmse <sanan_kazimov@outlook.com>
2020-01-10 08:54:56 +0400

    add activity edited

---
+++ src/main/java/az/gov/adra/dataTransferObjects/ActivityDTOForAddActivity.java
@@ -0,0 +1,19 @@
+package az.gov.adra.dataTransferObjects;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ActivityDTOForAddActivity {
+
+    private String title;
+    private String description;
+    private MultipartFile file;
+
+}
